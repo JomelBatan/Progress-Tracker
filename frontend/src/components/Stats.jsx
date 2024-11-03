e02afa994c82836e20773efb984b82cdb458abf2
Jomel Batan <jomelbatan6@gmail.com>
2024-11-04 03:07:13 +0800

    Final

--- frontend/src/components/Stats.jsx
+++ frontend/src/components/Stats.jsx
@@ -14,21 +14,29 @@ export default function Stats({ data, dateRange }) {
         hasStreak ? "grid-cols-2 md:grid-cols-4" : "grid-cols-2 md:grid-cols-3"
       }`}
     >
-      <div className="stat place-items-center bg-white rounded-lg p-4 shadow-md hover:shadow-lg transition">
-        <div className="stat-title text-gray-500">Days Passed</div>
-        <div className="stat-value text-2xl font-bold">{data.total}</div>
-        <div className="stat-desc text-gray-400">{dateRange}</div>
+      <div className="stat bg-white rounded-lg p-4 shadow-md hover:shadow-lg transition flex flex-col items-center">
+        <div className="stat-title text-gray-500 text-xs md:text-sm">
+          Days Passed
+        </div>
+        <div className="stat-value text-xl md:text-2xl font-bold">
+          {data.total}
+        </div>
+        <div className="stat-desc text-xs md:text-sm text-gray-400 text-center hidden md:block">
+          {dateRange}
+        </div>
       </div>
 
-      <div className="stat place-items-center bg-green-100 rounded-lg p-4 shadow-md hover:shadow-lg transition">
-        <div className="stat-title text-gray-500">Successful Days</div>
+      <div className="stat bg-green-100 rounded-lg p-4 shadow-md hover:shadow-lg transition flex flex-col items-center">
+        <div className="stat-title text-gray-500 text-xs md:text-sm">
+          Successful Days
+        </div>
         <div className="flex flex-row space-x-2 items-center">
-          <div className="stat-value text-2xl font-bold text-green-600">
+          <div className="stat-value text-xl md:text-2xl font-bold text-green-600">
             {data.success}
           </div>
           <MdSelfImprovement className="h-8 w-6 text-green-600" />
         </div>
-        <div className="stat-desc">
+        <div className="stat-desc text-xs md:text-sm text-center hidden md:block">
           <span
             className="tooltip tooltip-bottom text-gray-400"
             data-tip={`Success Rate: ${successRate}%`}
@@ -39,15 +47,17 @@ export default function Stats({ data, dateRange }) {
         </div>
       </div>
 
-      <div className="stat place-items-center bg-red-100 rounded-lg p-4 shadow-md hover:shadow-lg transition">
-        <div className="stat-title text-gray-500">Relapse Days</div>
+      <div className="stat bg-red-100 rounded-lg p-4 shadow-md hover:shadow-lg transition flex flex-col items-center">
+        <div className="stat-title text-gray-500 text-xs md:text-sm">
+          Relapse Days
+        </div>
         <div className="flex flex-row space-x-2 items-center">
-          <div className="stat-value text-2xl font-bold text-red-600">
+          <div className="stat-value text-xl md:text-2xl font-bold text-red-600">
             {data.fail}
           </div>
           <FaVolcano className="h-8 w-4 text-red-600" />
         </div>
-        <div className="stat-desc">
+        <div className="stat-desc text-xs md:text-sm text-center hidden md:block">
           <span
             className="tooltip tooltip-bottom text-gray-400"
             data-tip={`Fail Rate: ${failRate}%`}
@@ -59,78 +69,23 @@ export default function Stats({ data, dateRange }) {
       </div>
 
       {hasStreak && (
-        <div className="stat place-items-center bg-orange-100 rounded-lg p-4 shadow-md hover:shadow-lg transition">
-          <div className="stat-title text-gray-500">Current Streak</div>
+        <div className="stat bg-orange-100 rounded-lg p-4 shadow-md hover:shadow-lg transition flex flex-col items-center">
+          <div className="stat-title text-gray-500 text-xs md:text-sm">
+            Current Streak
+          </div>
           <div className="flex flex-row space-x-2 items-center">
-            <div className="stat-value text-2xl text-orange-400">
+            <div className="stat-value text-xl md:text-2xl text-orange-400">
               {data.streak}
             </div>
             <BsFire className="h-8 w-4 text-orange-400" />
           </div>
-          <div className="stat-desc text-gray-400">
-            Success Consecutive Days
+          <div className="stat-desc text-xs md:text-sm text-center hidden md:block">
+            <span className="tooltip tooltip-bottom text-gray-400">
+              Successful Days in a Row
+            </span>
           </div>
         </div>
       )}
     </div>
   );
 }
-
-// export default function Stats({ data, dateRange }) {
-//   const successRate = ((data.success / data.total) * 100).toFixed(2) || 0;
-//   const failRate = ((data.fail / data.total) * 100).toFixed(2) || 0;
-
-//   return (
-//     <div className="shadow mb-8 grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
-//       <div className="stat place-items-center bg-white rounded-lg p-4 shadow-md hover:shadow-lg transition">
-//         <div className="stat-title text-gray-500">Days Passed</div>
-//         <div className="stat-value text-2xl font-bold">{data.total}</div>
-//         <div className="stat-desc text-gray-400">{dateRange}</div>
-//       </div>
-
-//       <div className="stat place-items-center bg-green-100 rounded-lg p-4 shadow-md hover:shadow-lg transition">
-//         <div className="stat-title text-gray-500">Successful Days</div>
-//         <div className="stat-value text-2xl font-bold text-green-600">
-//           {data.success}
-//         </div>
-//         <div className="stat-desc">
-//           <span
-//             className="tooltip tooltip-bottom text-gray-400"
-//             data-tip={`Success Rate: ${successRate}%`}
-//           >
-//             {`${successRate}%`}{" "}
-//             <span className="text-gray-400">{`${data.success} days`}</span>
-//           </span>
-//         </div>
-//       </div>
-
-//       <div className="stat place-items-center bg-red-100 rounded-lg p-4 shadow-md hover:shadow-lg transition">
-//         <div className="stat-title text-gray-500">Relapse Days</div>
-//         <div className="stat-value text-2xl font-bold text-red-600">
-//           {data.fail}
-//         </div>
-//         <div className="stat-desc">
-//           <span
-//             className="tooltip tooltip-bottom text-gray-400"
-//             data-tip={`Fail Rate: ${failRate}%`}
-//           >
-//             {`${failRate}%`}{" "}
-//             <span className="text-gray-400">{`${data.fail} days`}</span>
-//           </span>
-//         </div>
-//       </div>
-
-//       {data.streak >= 1 && (
-//         <div className="stat place-items-center bg-orange-100 rounded-lg p-4 shadow-md hover:shadow-lg transition">
-//           <div className="stat-title text-gray-500">Current Streak</div>
-//           <div className="stat-value text-2xl text-orange-400">
-//             {data.streak}
-//           </div>
-//           <div className="stat-desc text-gray-400">
-//             Successful Days in a Row
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
